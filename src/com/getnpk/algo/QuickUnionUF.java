6c82b143bae7a4d0bcf2fd1504b6d356381d0243
getnpk <getnpk@gmail.com>
2014-02-10 15:16:36 +0800

    Weighted UF

--- src/com/getnpk/algo/QuickUnionUF.java
+++ src/com/getnpk/algo/QuickUnionUF.java
@@ -4,6 +4,7 @@ public class QuickUnionUF {
 
 	private int[] list;
 	
+	private int[] size;
 	/*
 	 * Initialize list with ids which form the root.
 	 * */
@@ -11,6 +12,10 @@ public class QuickUnionUF {
 		list = new int[n];
 		for (int i = 0; i < n ; i++)
 			list[i] = i;
+		
+		size = new int[n];
+		for (int i = 0; i < n; i++)
+			list[i] = 1;
 	}
 	
 	/*
@@ -32,5 +37,23 @@ public class QuickUnionUF {
 		int rb = root(b);
 		//change root of a to that of b.
 		list[ra] = rb;
+	}
+	
+	/*
+	 * Add smaller tree under root of bigger tree.
+	 * */
+	public void weightedUnion(int a, int b){
+		int ra = root(a);
+		int rb = root(b);
+		
+		if( ra == rb) return;
+		
+		if (size[ra] < size[rb]){
+			list[ra] = rb;
+			size[rb] += size[ra];
+		}else{
+			list[rb] = ra;
+			size[ra] += size[rb];
+		}
 	}
 }
