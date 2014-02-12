f73e8562e2f2e206c8b8ec228f8bca340495a511
getnpk <getnpk@gmail.com>
2014-02-12 16:10:44 +0800

    comprable, selection

---
+++ src/com/getnpk/algo/sort/Sort.java
@@ -0,0 +1,44 @@
+package com.getnpk.algo.sort;
+
+public class Sort {
+
+	
+	public static void selectionSort(Comparable[] a){
+		for (int i = 0; i < a.length; i++){
+			int min = i;
+			for (int j = i+1; j< a.length; j++){
+				if (lessThan(a[j], a[min]))
+					min = j;
+			}
+			swap(a , i , min);
+		}
+	}
+	
+	public static boolean lessThan(Comparable a, Comparable b){
+		return a.compareTo(b) == -1;
+	}
+	
+	public static void swap(Comparable[] array, int i, int j){
+		Comparable temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	public static void display(Comparable[] a){
+		for (Comparable c: a)
+			System.out.print(c.toString() + " ");
+		System.out.println();
+	}
+	
+	public static void main(String[] args){
+		
+		Comparable[] array = {7,1,2,4,6,3,5};
+		Comparable[] sarray = { "maverik","peter", "yo", "fido", "jackson"};
+		display(array);
+		display(sarray);
+		
+		selectionSort(array);
+		display(array);
+
+	}
+}
